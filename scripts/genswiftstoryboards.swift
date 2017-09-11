827e901fdb4bf9ff45bb28d80b0bfff7f16c39d8
Richard Burgess <rickb@rickb.com>
2017-09-11 15:26:52 -0500

    update

--- scripts/genswiftstoryboards.swift
+++ scripts/genswiftstoryboards.swift
@@ -85,6 +85,9 @@ func generateStoryboardIdentifierSourceFile(_ path: String) {
 		if let cs = try doc.nodes(forXPath:"//collectionViewController") as? [XMLElement] {
 			vcs.append(contentsOf: cs)
 		}
+		if let cs = try doc.nodes(forXPath:"//pageViewController") as? [XMLElement] {
+			vcs.append(contentsOf: cs)
+		}
 		guard vcs.count > 0 else {
 			return
 		}
