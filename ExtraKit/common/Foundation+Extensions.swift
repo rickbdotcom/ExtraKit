8e49e04db4917047270d2c97b28939c7bda80a15
Richard Burgess <rickb@rickb.com>
2017-11-24 01:11:23 -0600

    enum collection

--- ExtraKit/common/Foundation+Extensions.swift
+++ ExtraKit/common/Foundation+Extensions.swift
@@ -26,3 +26,29 @@ public extension FileManager {
 		return temporaryDirectory.appendingPathComponent(UUID().uuidString + (ext.flatMap{".\($0)"} ?? ""))
 	}
 }
+
+public protocol EnumCollection: Hashable {
+    static func cases() -> AnySequence<Self>
+    static var all: [Self] { get }
+}
+
+public extension EnumCollection {
+    
+    public static func cases() -> AnySequence<Self> {
+        return AnySequence { () -> AnyIterator<Self> in
+            var raw = 0
+            return AnyIterator {
+                let current: Self = withUnsafePointer(to: &raw) { $0.withMemoryRebound(to: self, capacity: 1) { $0.pointee } }
+                guard current.hashValue == raw else {
+                    return nil
+                }
+                raw += 1
+                return current
+            }
+        }
+    }
+    
+    public static var all: [Self] {
+        return Array(self.cases())
+    }
+}
