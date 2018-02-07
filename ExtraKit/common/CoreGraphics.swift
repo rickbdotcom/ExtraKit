20cfea4017bb29e93c4a980d0cd02454f1add9f3
Richard Burgess <rickb@rickb.com>
2018-02-07 14:17:13 -0600

    lint

--- ExtraKit/common/CoreGraphics.swift
+++ ExtraKit/common/CoreGraphics.swift
@@ -10,27 +10,27 @@ import CoreGraphics
 
 public extension CGSize {
 
-	enum aspectMode {
+	enum AspectMode {
 		case fit
 		case fill
 	}
 
-	func aspect(_ mode: aspectMode, in rect: CGRect) -> CGRect {
+	func aspect(_ mode: AspectMode, in rect: CGRect) -> CGRect {
 		let size = aspect(mode, in: rect.size)
 		return rect.insetBy(dx: (rect.size.width - size.width) / 2.0, dy: (rect.size.height - size.height) / 2.0)
 	}
 	
-	func aspect(_ mode: aspectMode, in size: CGSize) -> CGSize {
+	func aspect(_ mode: AspectMode, in size: CGSize) -> CGSize {
 		return scale(aspectScale(mode, in: size))
 	}
 	
-	func aspectScale(_ mode: aspectMode, in size: CGSize) -> CGFloat {
+	func aspectScale(_ mode: AspectMode, in size: CGSize) -> CGFloat {
 		let test = size.width * height > size.height * width
 		switch mode {
-			case .fill:
-				return test ? size.width / width : size.height / height
-			case .fit:
-				return test ? size.height / height : size.width / width
+		case .fill:
+			return test ? size.width / width : size.height / height
+		case .fit:
+			return test ? size.height / height : size.width / width
 		}
 	}
 
@@ -38,8 +38,8 @@ public extension CGSize {
 		return CGSize(width: width * x, height: height * y)
 	}
 	
-	func scale(_ s: CGFloat) -> CGSize {
-		return scale(x: s, y: s)
+	func scale(_ scale: CGFloat) -> CGSize {
+		return self.scale(x: scale, y: scale)
 	}
 }
 
@@ -53,7 +53,7 @@ public extension CGRect {
 		self.init(origin: .zero, size: size)
 	}
 	
-	init(x: CGFloat? = nil, y : CGFloat? = nil, width: CGFloat? = nil, height: CGFloat? = nil) {
+	init(x: CGFloat? = nil, y: CGFloat? = nil, width: CGFloat? = nil, height: CGFloat? = nil) {
 		self.init(x: x ?? 0, y: y ?? 0, width: width ?? 0, height: height ?? 0)
 	}
 	
@@ -106,8 +106,8 @@ public extension CGRect {
 		return CGRect(origin: origin.scale(x: x, y: y), size: size.scale(x: x, y: y))
 	}
 	
-	func scale(_ s: CGFloat) -> CGRect {
-		return scale(x: s, y: s)
+	func scale(_ scale: CGFloat) -> CGRect {
+		return self.scale(x: scale, y: scale)
 	}
 }
 
@@ -117,24 +117,24 @@ public extension CGPoint {
 		return CGPoint(x: self.x * x, y: self.y * y)
 	}
 	
-	func scale(_ s: CGFloat) -> CGPoint {
-		return scale(x: s, y: s)
+	func scale(_ scale: CGFloat) -> CGPoint {
+		return self.scale(x: scale, y: scale)
 	}
 }
 
 public extension CGContext {
 
-	func pushGState(block: ()->Void) {
+	func pushGState(block: () -> Void) {
 		saveGState()
 		block()
 		restoreGState()
 	}
 	
-	func rotate(at: CGPoint, by: CGFloat, block: (()->Void)? = nil) {
+	func rotate(at point: CGPoint, by offset: CGFloat, block: (() -> Void)? = nil) {
 		pushGState {
-			translateBy(x: at.x, y: at.y)
-			rotate(by: by)
-			translateBy(x: -at.x, y: -at.y)
+			translateBy(x: point.x, y: point.y)
+			rotate(by: offset)
+			translateBy(x: -point.x, y: -point.y)
 			block?()
 		}
 	}
