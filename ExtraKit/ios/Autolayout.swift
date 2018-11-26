9a992869bdb2c6343fc4a840e02cced4f8c2423d
rickb <rickb@rickb.com>
2018-11-26 17:00:04 -0600

    priority

--- ExtraKit/ios/Autolayout.swift
+++ ExtraKit/ios/Autolayout.swift
@@ -32,33 +32,33 @@ public extension AnchorableObject {
 }
 public extension UIView {
 
-	@discardableResult func pin(edges: UIRectEdge = .all, to view: AnchorableObject? = nil, with insets: UIEdgeInsets = .zero, alignWithLanguageDirection: Bool = false) -> Self {
-		pinConstraints(edges: edges, to: view, with: insets, alignWithLanguageDirection: alignWithLanguageDirection)
+	@discardableResult func pin(edges: UIRectEdge = .all, to view: AnchorableObject? = nil, with insets: UIEdgeInsets = .zero, alignWithLanguageDirection: Bool = false, priority: UILayoutPriority = .required) -> Self {
+		pinConstraints(edges: edges, to: view, with: insets, alignWithLanguageDirection: alignWithLanguageDirection, priority: priority)
 		return self
 	}
 
-	@discardableResult func center(to view: UIView? = nil, offset: CGPoint = .zero) -> Self {
-		centerConstraints(to: view, offset: offset)
+	@discardableResult func center(to view: UIView? = nil, offset: CGPoint = .zero, priority: UILayoutPriority = .required) -> Self {
+		centerConstraints(to: view, offset: offset, priority: priority)
 		return self
 	}
 	
-	@discardableResult func size(to size: CGSize) -> Self {
-		sizeConstraints(to: size)
+	@discardableResult func size(to size: CGSize, priority: UILayoutPriority = .required) -> Self {
+		sizeConstraints(to: size, priority: priority)
 		return self
 	}
 	
-	@discardableResult func aspectRatio(_ ratio: CGFloat) -> Self {
-		aspectRatioConstraint(ratio)
+	@discardableResult func aspectRatio(_ ratio: CGFloat, priority: UILayoutPriority = .required) -> Self {
+		aspectRatioConstraint(ratio, priority: priority)
 		return self
 	}
 
-	@discardableResult func aspectRatioConstraint(_ ratio: CGFloat) -> NSLayoutConstraint {
-		let constraint = widthAnchor.constraint(equalTo: heightAnchor, multiplier: ratio)
+	@discardableResult func aspectRatioConstraint(_ ratio: CGFloat, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+		let constraint = widthAnchor.constraint(equalTo: heightAnchor, multiplier: ratio).configure { $0.priority = priority }
 		constraint.isActive = true
 		return constraint
 	}
 	
-	@discardableResult func pinConstraints(edges: UIRectEdge = .all, to view: AnchorableObject? = nil, with insets: UIEdgeInsets = .zero, alignWithLanguageDirection: Bool = false) -> [NSLayoutConstraint] {
+	@discardableResult func pinConstraints(edges: UIRectEdge = .all, to view: AnchorableObject? = nil, with insets: UIEdgeInsets = .zero, alignWithLanguageDirection: Bool = false, priority: UILayoutPriority = .required) -> [NSLayoutConstraint] {
 		translatesAutoresizingMaskIntoConstraints = false
 		guard let pinToView = view ?? superview else {
 			return []
@@ -66,38 +66,38 @@ public extension UIView {
 
 		var constraints = [NSLayoutConstraint]()
 		if edges.contains(.top) {
- 			constraints.append(pinToView.topAnchor.constraint(equalTo: topAnchor, constant: -insets.top))
+ 			constraints.append(pinToView.topAnchor.constraint(equalTo: topAnchor, constant: -insets.top).configure { $0.priority = priority })
 		}
 		if edges.contains(.left) {
-			constraints.append(pinToView.xAxisStartAnchor(alignWithLanguageDirection: alignWithLanguageDirection).constraint(equalTo: xAxisStartAnchor(alignWithLanguageDirection: alignWithLanguageDirection), constant: -insets.left))
+			constraints.append(pinToView.xAxisStartAnchor(alignWithLanguageDirection: alignWithLanguageDirection).constraint(equalTo: xAxisStartAnchor(alignWithLanguageDirection: alignWithLanguageDirection), constant: -insets.left).configure { $0.priority = priority })
 		}
 		if edges.contains(.bottom) {
-			constraints.append(pinToView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: insets.bottom))
+			constraints.append(pinToView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: insets.bottom).configure { $0.priority = priority })
 		}
 		if edges.contains(.right) {
-			constraints.append(pinToView.xAxisEndAnchor(alignWithLanguageDirection: alignWithLanguageDirection).constraint(equalTo: xAxisEndAnchor(alignWithLanguageDirection: alignWithLanguageDirection), constant: insets.right))
+			constraints.append(pinToView.xAxisEndAnchor(alignWithLanguageDirection: alignWithLanguageDirection).constraint(equalTo: xAxisEndAnchor(alignWithLanguageDirection: alignWithLanguageDirection), constant: insets.right).configure { $0.priority = priority })
 		}
 		NSLayoutConstraint.activate(constraints)
 		return constraints
 	}
 
-	@discardableResult func centerConstraints(to view: UIView? = nil, offset: CGPoint = .zero) -> [NSLayoutConstraint] {
+	@discardableResult func centerConstraints(to view: UIView? = nil, offset: CGPoint = .zero, priority: UILayoutPriority = .required) -> [NSLayoutConstraint] {
 		translatesAutoresizingMaskIntoConstraints = false
 		guard let centerToView = view ?? superview else {
 			return []
 		}
 		var constraints = [NSLayoutConstraint]()
-		constraints.append(centerToView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: offset.x))
-		constraints.append(centerToView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: offset.y))
+		constraints.append(centerToView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: offset.x).configure { $0.priority = priority })
+		constraints.append(centerToView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: offset.y).configure { $0.priority = priority })
 
 		NSLayoutConstraint.activate(constraints)
 		return constraints
 	}
 		
-	@discardableResult func sizeConstraints(to size: CGSize) -> [NSLayoutConstraint] {
+	@discardableResult func sizeConstraints(to size: CGSize, priority: UILayoutPriority = .required) -> [NSLayoutConstraint] {
 		var constraints = [NSLayoutConstraint]()
-		constraints.append(widthAnchor.constraint(equalToConstant: size.width))
-		constraints.append(heightAnchor.constraint(equalToConstant: size.height))
+		constraints.append(widthAnchor.constraint(equalToConstant: size.width).configure { $0.priority = priority })
+		constraints.append(heightAnchor.constraint(equalToConstant: size.height).configure { $0.priority = priority })
 		NSLayoutConstraint.activate(constraints)
 		return constraints
 	}
