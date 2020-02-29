55a25b704bdb43a4bbc6b70166a6d8c81abb45ab
Rick <rickb@rickb.com>
2020-02-29 15:29:25 -0600

    update

--- Sources/ExtraKitUI/UIKit.swift
+++ Sources/ExtraKitUI/UIKit.swift
@@ -2,47 +2,27 @@ import UIKit
 
 public extension UIViewController {
 
-	func parentViewController<T>(ofType: T.Type) -> T? {
-		return typedParentViewController()
+	func parentViewController<T>(ofType: T.Type = T.self) -> T? {
+		self as? T ?? parent?.parentViewController()
 	}
 	
-	func childViewController<T>(ofType: T.Type) -> T? {
-		return typedChildViewController()
-	}
-
-	func typedParentViewController<T>() -> T? {
-		return self as? T ?? parent?.typedParentViewController()
-	}
-	
-	func typedChildViewController<T>() -> T? {
-		return children.first(where: { $0 is T }) as? T
+	func childViewController<T>(ofType: T.Type = T.self) -> T? {
+		children.first(where: { $0 is T }) as? T
 	}
 }
 
 public extension UIView {
 
-    func superview<T>(ofType: T.Type) -> T? {
-        return typedSuperview()
+    func superview<T>(ofType: T.Type = T.self) -> T? {
+        self as? T ?? superview?.superview()
     }
 
-    func parentViewController<T>(ofType: T.Type) -> T? {
-        return typedParentViewController()
+    func parentViewController<T>(ofType: T.Type = T.self) -> T? {
+        self as? T ?? parentViewController?.parentViewController()
     }
 
-    func subview<T>(ofType: T.Type) -> T? {
-        return typedSubview()
-    }
-
-    func typedSuperview<T>() -> T? {
-        return self as? T ?? superview?.typedSuperview()
-    }
-
-    func typedParentViewController<T>() -> T? {
-        return self as? T ?? parentViewController?.typedParentViewController()
-    }
-
-    func typedSubview<T>() -> T? {
-        return subviews.first { $0 is T } as? T
+    func subview<T>(ofType: T.Type = T.self) -> T? {
+		subviews.first { $0 is T } as? T
     }
 
     var parentViewController: UIViewController? {
@@ -57,71 +37,6 @@ public extension UIView {
     }
 }
 
-public extension UIView {
-	
-	@IBInspectable var borderColor: UIColor? {
-		get {
-			return layer.borderColor != nil ? UIColor(cgColor: layer.borderColor!) : nil
-		}
-		set {
-			layer.borderColor = newValue?.cgColor
-		}
-	}
-	
-	@IBInspectable var cornerRadius: CGFloat {
-		get {
-			return layer.cornerRadius
-		}
-		set {
-			layer.masksToBounds = true
-			layer.cornerRadius = newValue
-		}
-	}
-
-	@IBInspectable var borderWidth: CGFloat {
-		get {
-			return layer.borderWidth
-		}
-		set {
-			layer.borderWidth = newValue
-		}
-	}
-	
-	@IBInspectable var maskedCornersString: String? {
-		get {
-			if #available(iOS 11.0, *) {
-				return maskedCornerArray.map {
-					layer.maskedCorners.contains($0) ? "true" : "false"
-				}.joined(separator: " ")
-			} else {
-				return nil
-			}
-		}
-		set {
-			if #available(iOS 11.0, *) {
-				if let boolArray = newValue?.components(separatedBy: " ").map({ $0 == "true" }), boolArray.count == 4 {
-					layer.masksToBounds = true
-					layer.maskedCorners = zip(maskedCornerArray, boolArray).reduce([]) {
-						return $1.1 ? $0.union($1.0) : $0
-					}
-				} else {
-					layer.maskedCorners = []
-				}
-			}
-		}
-	}
-}
-
-private let maskedCornerArray: [CACornerMask] = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-
-public extension UIView {
-
-	@IBOutlet weak var containerViewOrSelf: UIView! {
-		get { return weakAssociatedValue() ?? self }
-		set { set(weakAssociatedValue: newValue) }
-	}
-}
-
 public extension UIEdgeInsets {
 
 	init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil) {
@@ -130,24 +45,5 @@ public extension UIEdgeInsets {
 	
 	init(_ inset: CGFloat) {
 		self.init(top: inset, left: inset, bottom: inset, right: inset)
-	}
-}
-
-public extension UIView {
-
-	@IBInspectable var clearBackground: Bool {
-		get { return associatedValue() ?? false }
-		set { set(associatedValue: newValue) }
-	}
-	
-	class func useClearBackground() {
-		swizzle(instanceMethod: #selector(awakeFromNib), with: #selector(clearBackground_awakeFromNib))
-	}
-	
-	@objc func clearBackground_awakeFromNib() {
-		clearBackground_awakeFromNib()
-		if clearBackground {
-			backgroundColor = .clear
-		}
 	}
 }
